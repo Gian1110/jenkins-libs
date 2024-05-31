bae6b16e42514242b5382d4d51e3a1393316b52a
Gianluca Berarducci <175530@coto.com.ar>
2024-05-31 10:20:08 -0300

    ssh

--- vars/dockerb.groovy
+++ vars/dockerb.groovy
@@ -16,23 +16,23 @@ def initial(String remoteHost){
     return remoteH;
 }
 
-def dockerpull(Map params){
-    //def remoteHost = initial(params.remoteHost);
-    //echo "${remoteHost}"
-    def sshId = ConfigJenkins.getSshCredentialId();
-    def remote = [:]
+// def dockerpull(Map params){
+//     //def remoteHost = initial(params.remoteHost);
+//     //echo "${remoteHost}"
+//     def sshId = ConfigJenkins.getSshCredentialId();
+//     def remote = [:]
 
-    withCredentials([usernamePassword(credentialsId: sshId, usernameVariable: 'sshUser', passwordVariable: 'sshpass')]){
+//     withCredentials([usernamePassword(credentialsId: sshId, usernameVariable: 'sshUser', passwordVariable: 'sshpass')]){
                         
-            remote["name" : "pipe",
-            "host":"${params.remoteHost}",
-            "user" : "${sshUser}",
-            "password" : "${sshpass}",
-            "allowAnyHosts" :  true] 
-            sshCommand remote: "${remote}", command: "docker ps"
-    }
+//             remote["name" : "pipe",
+//             "host":"${params.remoteHost}",
+//             "user" : "${sshUser}",
+//             "password" : "${sshpass}",
+//             "allowAnyHosts" :  true] 
+//             sshCommand remote: "${remote}", command: "docker ps"
+//     }
     
      
-    return params;
-}
+//     return params;
+// }
 //docker pull ${params.nameImagen}
