1af0c808262fd3a64c24dcf3c9ac67bb74ae1ff4
Gianluca Berarducci <175530@coto.com.ar>
2024-05-31 09:04:35 -0300

    cambios

--- vars/dockerb.groovy
+++ vars/dockerb.groovy
@@ -15,8 +15,20 @@ def initial(String remoteHost){
 }
 
 def dockerpull(Map params){
-    def remoteHost = initial(params.remoteHost);
-    echo "${remoteHost}"
-    sshCommand remote: "${remoteHost}", command: "docker pull ${params.nameImagen}";
+    //def remoteHost = initial(params.remoteHost);
+    //echo "${remoteHost}"
+    def sshId = ConfigJenkins.getSshCredentialId();
+    def remote = [:]
+
+    withCredentials([usernamePassword(credentialsId: sshId, usernameVariable: 'sshUser', passwordVariable: 'sshpass')]){
+                        
+            remote["name"] = "pipe"
+            remote["host"] = params.remoteHost
+            remote["user"] = "${sshUser}"
+            remote["password"] = "${sshpass}"
+            remote["allowAnyHosts"] =  true
+
+    }  
+    sshCommand remote: "${remote}", command: "docker pull ${params.nameImagen}";
     return params;
 }
