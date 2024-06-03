2b46498eda5873fec3a6bb9d85a9b9456d7c316e
Gianluca Berarducci <175530@coto.com.ar>
2024-06-03 14:17:05 -0300

    anda lo anterior, cambio de registry

--- src/ConfigJenkins.groovy
+++ src/ConfigJenkins.groovy
@@ -10,4 +10,7 @@ public class ConfigJenkins {
         return sshCredentialId;
     }
 
+    public static String getImagenRegistry(String nameContainer,String versionImagen){
+        return registryURL + "/" + nameContainer + ":" + versionImagen ;
+    }
 }
