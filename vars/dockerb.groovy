0935d2998b57e33874853f618d8105301ead6e99
Gianluca Berarducci <175530@coto.com.ar>
2024-06-07 11:06:51 -0300

    try en todos las funciones

--- vars/dockerb.groovy
+++ vars/dockerb.groovy
@@ -15,38 +15,53 @@ def initial(String remoteHost){
 }
 
 def dockerVersionContainer(Map params) {
- 
-    def remoteH = initial(params.remoteHost)
-    def imageVersion = sshCommand remote: remoteH, command: "docker ps -a --format '{{.Image}}'| grep ${params.containerName}"
-    imageVersion = imageVersion.split(":")[2]
+    try{
+        def remoteH = initial(params.remoteHost)
+        def imageVersion = sshCommand remote: remoteH, command: "docker ps -a --format '{{.Image}}'| grep ${params.containerName}"
+        imageVersion = imageVersion.split(":")[2]
+    } catch(Exception e) {
+        echo "${e}"
+    }    
     return  imageVersion == params.imagenVersion
+
 }
 
 def dockerBuildPush(Map params){
-    def nameImage = ConfigJenkins.getImagenRegistry(params.containerName,params.imagenVersion);
-    dir (params.path) {
-        sh " docker build -t ${nameImage} ."
-    }
-    sh """
-        docker push ${nameImage}
-        docker rmi ${nameImage}
-      """
+    try{
+        def nameImage = ConfigJenkins.getImagenRegistry(params.containerName,params.imagenVersion);
+        dir (params.path) {
+            sh " docker build -t ${nameImage} ."
+        }
+        sh """
+            docker push ${nameImage}
+            docker rmi ${nameImage}
+        """
+     } catch(Exception e) {
+        echo "${e}"
+    }  
 }
 
 def dockerPull(Map params){
-    def nameImage = ConfigJenkins.getImagenRegistry(params.containerName,params.imagenVersion);
-    def remoteH = initial(params.remoteHost);
-    sshCommand remote: remoteH, command: "docker pull ${nameImage}"
-
+    try{
+        def nameImage = ConfigJenkins.getImagenRegistry(params.containerName,params.imagenVersion);
+        def remoteH = initial(params.remoteHost);
+        sshCommand remote: remoteH, command: "docker pull ${nameImage}"
+    } catch(Exception e) {
+        echo "${e}"
+    }
 }
 
 def dockerRmRun(Map params) {
-    def remoteH = initial(params.remoteHost);
-    def nameImage = ConfigJenkins.getImagenRegistry(params.containerName,params.imagenVersion);
-    def DOCKER_EXIST = sshCommand remote: remoteH, command: "docker ps -a -q --filter name=${params.containerName}"
+    try{
+        def remoteH = initial(params.remoteHost);
+        def nameImage = ConfigJenkins.getImagenRegistry(params.containerName,params.imagenVersion);
+        def DOCKER_EXIST = sshCommand remote: remoteH, command: "docker ps -a -q --filter name=${params.containerName}"
 
-    if (DOCKER_EXIST != ''){
-        sshCommand remote: remoteH, command: "docker rm -f ${params.containerName}"
-    }               
-    sshCommand remote: remoteH, command: "docker run -d -p ${params.containerPuert}:80 --name ${params.containerName} ${nameImage}"
+        if (DOCKER_EXIST != ''){
+            sshCommand remote: remoteH, command: "docker rm -f ${params.containerName}"
+        }               
+        sshCommand remote: remoteH, command: "docker run -d -p ${params.containerPuert}:80 --name ${params.containerName} ${nameImage}"
+    } catch(Exception e) {
+        echo "${e}"
+    }   
 }
