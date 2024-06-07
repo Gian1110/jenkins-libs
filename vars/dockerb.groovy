61d194bcb1b5b17f15c72daad3fc47316f0a04ff
Gianluca Berarducci <175530@coto.com.ar>
2024-06-07 09:17:45 -0300

    echos

--- vars/dockerb.groovy
+++ vars/dockerb.groovy
@@ -18,6 +18,10 @@ def dockerVersionContainer(Map params) {
     def nameImage = ConfigJenkins.getImagenRegistry(params.containerName,params.imagenVersion);
     def imageVersion = sshCommand remote: remoteH, command: "docker ps -a --format '{{.Image}}'| grep ${params.containerName}"
     imageVersion = imageVersion.split(":")[1]
+    echo "${imageVersion}"
+    echo "${params.imageVersion}"
+    def valor = imageVersion == params.imageVersion
+    echo "${valor}"
     return  imageVersion == params.imageVersion;
 }
 
