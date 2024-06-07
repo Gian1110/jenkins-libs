f85729e6b81970cc3c3bcbc427d9340d95cd1396
Gianluca Berarducci <175530@coto.com.ar>
2024-06-07 11:10:38 -0300

    try correcction

--- vars/dockerb.groovy
+++ vars/dockerb.groovy
@@ -15,9 +15,10 @@ def initial(String remoteHost){
 }
 
 def dockerVersionContainer(Map params) {
+    def imageVersion = ""
     try{
         def remoteH = initial(params.remoteHost)
-        def imageVersion = sshCommand remote: remoteH, command: "docker ps -a --format '{{.Image}}'| grep ${params.containerName}"
+        imageVersion = sshCommand remote: remoteH, command: "docker ps -a --format '{{.Image}}'| grep ${params.containerName}"
         imageVersion = imageVersion.split(":")[2]
     } catch(Exception e) {
         echo "${e}"
